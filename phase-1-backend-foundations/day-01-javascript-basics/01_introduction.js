21ceff74deacae7e8af6bd22c4b5377562c293f4
kabiryadav0932 <kabiryadav0932@gmail.com>
2025-12-20 21:40:14 +0530

    hoisting

---
+++ phase-1-backend-foundations/day-01-javascript-basics/01_introduction.js
@@ -0,0 +1,29 @@
+/*                                   javaScript :-
+
+Everything in jS happens inside an "Execution Context".
+
+
+
++--------+-----+-------+
+| Memory     |  Code   |
++--------+-----+-------+
+| a : 10     |   o     |
+| fun : {___}|   o     |
++--------+-----+-------+
+
+- here anothe name for 'memory' is variable environment, wherre all the key:value paired stored and
+  for code, another name is 'Thread of Execution', where every code executed one line at a time.
+
+
+- jS is a synchronous single-threaded language 
+meaning - it can execute one command at a time, then after it goes for next command.
+
+- callstack maintains the order of Execution of execution contexts
+ There are many names for callstack :-
+ a) Program Stack
+ b) Execution Context Stack
+ c) Control Stack
+ d) Runtime Stack
+ e) Machine Stack
+
+*/
