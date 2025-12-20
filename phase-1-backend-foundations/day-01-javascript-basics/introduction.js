9ff8724f4f5b23fb9f9aa2bbc65901c6f3d3bc9e
kabiryadav0932 <kabiryadav0932@gmail.com>
2025-12-20 20:22:50 +0530

    introduction

---
+++ phase-1-backend-foundations/day-01-javascript-basics/introduction.js
@@ -0,0 +1,25 @@
+/*
+
+                                    javaScript :-
+
+Everything in jS happens inside an "Execution Context".
+
+
+
++--------+-----+-------+
+| Memory     |  Code   |
++--------+-----+-------+
+| a : 10     |   o     |
+| fun : {___}|   o     |
++--------+-----+-------+
+
+- here anothe name for 'memory' is variable environment, wherre all the key:value paired stored and
+  for code, another name is 'Thread of Execution', where every code executed one line at a time.
+
+
+- jS is a synchronous single-threaded language 
+meaning - it can execute one command at a time, then after it goes for next command.
+
+
+
+*/
