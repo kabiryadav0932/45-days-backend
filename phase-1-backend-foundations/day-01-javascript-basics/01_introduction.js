8dde376527c11c74876931bf09a7096c39fc0b7e
kabiryadav0932 <kabiryadav0932@gmail.com>
2025-12-21 17:14:35 +0530

    arrow function

--- phase-1-backend-foundations/day-01-javascript-basics/01_introduction.js
+++ phase-1-backend-foundations/day-01-javascript-basics/01_introduction.js
@@ -14,7 +14,7 @@ Everything in jS happens inside an "Execution Context".
 - here anothe name for 'memory' is variable environment, wherre all the key:value paired stored and
   for code, another name is 'Thread of Execution', where every code executed one line at a time.
 
-
+  
 - jS is a synchronous single-threaded language 
 meaning - it can execute one command at a time, then after it goes for next command.
 
---
+++ phase-1-backend-foundations/day-01-javascript-basics/05_arrow_function.js
@@ -0,0 +1,14 @@
+
+// NORMAL FUNCTION
+function addTwonumber(number1, number2) {
+    return number1+number2;
+}
+
+console.log("answer arrow :", addTwonumber(2 , 5));
+
+
+
+// ARROW FUNCTION
+const addTwonumbers = (number1, number2) => { return number1 + number2; }
+
+console.log("answer arrow :", addTwonumbers(2 , 7));
