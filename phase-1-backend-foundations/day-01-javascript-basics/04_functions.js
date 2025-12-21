8033cd38275086e813e400e2a67da250fb4139ac
kabiryadav0932 <kabiryadav0932@gmail.com>
2025-12-21 11:34:43 +0530

    function

---
+++ phase-1-backend-foundations/day-01-javascript-basics/04_functions.js
@@ -0,0 +1,18 @@
+function addTwonumbers(number1, number2){
+
+    if(number1,number2 <=0){
+        console.log("Enter positive number");
+    } else { return number1+number2 }
+
+   // console.log(number1 + number2);
+   // return number1+number2;
+
+}
+
+console.log("answer :",addTwonumbers(5, -4));
+
+// this is the syntax of a function, here the name of the function is addTwonumbers and, we are passing two parameters here(number1 and number2)
+// after writing the function, now its time to invoke it, function will be there, and it will not execute on its own, we have to invoke it, to invoke the function we have to write the name of the function and also if there is some parameter there then we have to pass the argument at the time of the invoke.
+
+
+// there are many ways in which a function can be write.
